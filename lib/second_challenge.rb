71b9e722f1895bdf202bc5684ca90042b64e4118
Matoya Williams <matoyawilliams@yahoo.com>
2020-02-02 00:46:17 +0000

    Done.

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,19 +1,16 @@
 def second_challenge
-  epic_tragedy = {
-    :montague => {
-           :patriarch => {},
+epic_tragedy = {
+   :montague => {
+      :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :hero_friends => 
-      []
-      }, 
-    :capulet => {
-         :patriarch => {},
+      :hero_friends => []
+   },
+   :capulet => {
+      :patriarch => {},
       :matriarch => {},
       :heroine => {},
       :heroine_friends => []
-      }
-     }
-
-  
+   }
+}
 end
