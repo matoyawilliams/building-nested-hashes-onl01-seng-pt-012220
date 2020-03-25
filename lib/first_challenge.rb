c68d1b4fdc5821348fd928b1c07367267bea616f
Matoya Williams <matoyawilliams@yahoo.com>
2020-03-25 23:22:53 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,14 @@
+<<<<<<< HEAD
 def first_challenge
   epic_tragedy = {
   :montague => {},
   :capulet => {}
 }
+=======
+def first_challenge
+  epic_tragedy = { 
+  :montague => {},
+  :capulet => {}
+}
+>>>>>>> ba96ddb901a7a166cca9b8598e67cea423b683e8
 end
--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,4 +1,5 @@
 def second_challenge
+<<<<<<< HEAD
 epic_tragedy = {
    :montague => {
       :patriarch => {},
@@ -14,3 +15,23 @@ epic_tragedy = {
    }
 }
 end
+=======
+  epic_tragedy = {
+    :montague => {
+           :patriarch => {},
+      :matriarch => {},
+      :hero => {},
+      :hero_friends => 
+      []
+      }, 
+    :capulet => {
+         :patriarch => {},
+      :matriarch => {},
+      :heroine => {},
+      :heroine_friends => []
+      }
+     }
+
+  
+end
+>>>>>>> ba96ddb901a7a166cca9b8598e67cea423b683e8
